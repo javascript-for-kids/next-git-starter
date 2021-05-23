c7b3a39531c0c25ebfdd808293e4af33c542c1fc
Lautaro Gruss <lautapercuspain@gmail.com>
2021-05-22 23:51:11 -0300

    tina cloud

--- pages/admin/posts/[slug].js
+++ pages/admin/posts/[slug].js
@@ -1,6 +1,49 @@
+// pages/admin/posts/[slug].js
 import React from 'react'
+import { useGraphqlForms } from 'tina-graphql-gateway'
+import { useRouter } from 'next/router'
 
-export default function() {
+export default function BlogPostEditor() {
+  const query = gql => gql`
+    query BlogPostQuery($relativePath: String!) {
+      getPostsDocument(relativePath: $relativePath) {
+        data {
+          __typename
+          ... on Post_Doc_Data {
+            title
+            excerpt
+            coverImage
+            date
+            author {
+              name
+              picture
+            }
+            ogImage {
+              url
+            }
+            _body
+          }
+        }
+      }
+    }
+  `
+
+  const router = useRouter()
+  const [payload, isLoading] = useGraphqlForms({
+    query,
+    variables: {
+      relativePath: `${router.query.slug}.md`,
+    },
+    formify: ({ createForm, formConfig }) => {
+      formConfig.fields?.forEach(field => {
+        //use markdown plugin with _body field
+        if (field.name === '_body') {
+          field.component = 'markdown'
+        }
+      })
+      return createForm(formConfig)
+    },
+  })
+
   return <div>My admin page</div>
 }
-
